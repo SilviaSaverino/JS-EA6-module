24900c8787322a41817e02152b89e8224b4b0fbd
Silvia Saverino <silviasave89@gmail.com>
2022-12-01 17:10:15 +0000

    add content to arrow-functions file

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,47 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a,b) {
+    //code block
+    return a + b
+}
+
+let sum = addTwoNumbers(3,5);   //function stored in a variable
+console.log(sum);
 
 // Arrow Function With Parameters
+/*
+const addTwoNumbers = (a,b) => {
+    //code block
+    return a + b
+}
+
+let sum = addTwoNumbers(3,5);   
+console.log(sum);
+*/
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a,b) => (a + b);  also valid within parenthesis
+const addTwoNumbers2 = (a,b) => a + b;
+
+let sum2 = addTwoNumbers2(3,7);   
+console.log(sum2);
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Hello');
+
+const sayHello = () => console.log('Hola');
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+   `
+   <p>
+        This is a multiline string
+    </p> ` 
+);
+
+console.log(returnMultipleLines());
